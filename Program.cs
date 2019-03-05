b54cd5786a8c9a1711e17f54883007217a0c58ab
jojokabin <egorushkanutslove@gmail.com>
2019-03-05 12:40:55 +0700

    del

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите А: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите действие:");
+
+            ConsoleKeyInfo Key = Console.ReadKey();
+
+
+
+            char selection = Key.KeyChar;
+            switch (selection)
+            {
+                case '/':
+                    {
+                        int x = a;
+                        int y = b;
+
+                        int z = x / y;
+                        Console.WriteLine(z);
+                        break;
+                    }
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
